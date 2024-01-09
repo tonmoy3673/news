3a6028f48a6c3ef58bd1c026b532d12f6d833ccd
tonmoy <tonmoy3673@gmail.com>
2024-01-09 13:37:59 +0600

    set router

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "./components/Main/Main.jsx";
+import Home from "./components/Pages/Home/Home/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
