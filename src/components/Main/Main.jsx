34c0badd47f3d314f8b8da441666afe8ad5672f3
tonmoy <tonmoy3673@gmail.com>
2024-01-10 19:31:23 +0600

    added layOut

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,21 @@
+import { Col, Container, Row } from "react-bootstrap";
+import Footer from "../Pages/Shared/Footer";
+import Header from "../Pages/Shared/Header";
+
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Container>
+        <Row>
+          <Col lg={3}>sm=8</Col>
+          <Col lg={6}>Main Content Coming Soon</Col>
+          <Col lg={3}>sm=4</Col>
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
