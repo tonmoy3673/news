7791ae0fb8d9abecd2e5ccae1082b24d6c642c92
tonmoy <tonmoy3673@gmail.com>
2024-01-11 16:22:10 +0600

    added moment js & format date

--- src/components/Pages/Shared/Header.jsx
+++ src/components/Pages/Shared/Header.jsx
@@ -1,10 +1,14 @@
 import logo from "../../../assets/logo.png";
+import moment from "moment";
 
 const Header = () => {
   return (
     <div className="text-center py-3">
       <img src={logo} alt="logo" className="py-2 img-fluid" />
-      <p>Journalism Without Fear or Favour</p>
+      <h5>Journalism Without Fear or Favour</h5>
+      <p>
+        <small>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</small>
+      </p>
     </div>
   );
 };
