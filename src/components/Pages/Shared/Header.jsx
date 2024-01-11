1fd7de7a608f154b6aa05f0b1f21c68cc06242b7
tonmoy <tonmoy3673@gmail.com>
2024-01-11 17:54:19 +0600

    added two button

--- src/components/Pages/Shared/Header.jsx
+++ src/components/Pages/Shared/Header.jsx
@@ -1,4 +1,4 @@
-import { Button } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
 import Marquee from "react-fast-marquee";
@@ -7,13 +7,15 @@ const Header = () => {
   return (
     <div className="text-center py-3 container">
       <img src={logo} alt="logo" className="py-2 img-fluid" />
-      <h5>Journalism Without Fear or Favour</h5>
+      <h5 className="text-secondary">Journalism Without Fear or Favour</h5>
       <p>
         <small>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</small>
       </p>
+
+      {/* ============= banner ============= */}
       <div className="d-flex justify-content-center align-items-center bg-light rounded-2">
         <Button variant="danger">Latest</Button>
-        <Marquee className="mt-3" speed={70}>
+        <Marquee className="mt-3" speed={80}>
           <p className="me-4">
             In a major stride towards a sustainable future, a team of
             scientists....
@@ -33,6 +35,35 @@ const Header = () => {
           </p>
         </Marquee>
       </div>
+
+      {/* ========================= Nav bar =================== */}
+      <div className="py-4">
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav ">
+              <Nav className="mx-auto">
+                <Nav.Link href="#features" className="text-primary">
+                  Home
+                </Nav.Link>
+                <Nav.Link href="#pricing" className="text-primary">
+                  About
+                </Nav.Link>
+                <Nav.Link href="#pricing" className="text-primary">
+                  Career
+                </Nav.Link>
+              </Nav>
+              <Nav className="d-flex justify-content-center align-items-center">
+                <Nav.Link href="#deets">Profile</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                  <Button variant="primary">Login</Button>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      {/* ====================== Nav bar =================== */}
     </div>
   );
 };
--- src/components/Pages/Shared/RightNav/RightNav.jsx
+++ src/components/Pages/Shared/RightNav/RightNav.jsx
@@ -1,7 +1,25 @@
+import { Button } from "react-bootstrap";
+import { FaGoogle } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa6";
+
 const RightNav = () => {
   return (
     <div>
-      <h3> This is Right Nav</h3>
+      <h5 className="text-secondary text-center mb-3"> Login With</h5>
+      <div className="text-center">
+        <div className="mb-2">
+          <Button variant="outline-primary" className="w-50">
+            <FaGoogle className="me-2" />
+            Google
+          </Button>
+        </div>
+        <div>
+          <Button variant="outline-success" className="w-50">
+            <FaFacebookF className="me-2" />
+            Facebook
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
