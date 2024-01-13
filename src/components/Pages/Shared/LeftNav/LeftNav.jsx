8637bf7e69fdc9e600c15998f7462bf139b6d608
tonmoy <tonmoy3673@gmail.com>
2024-01-13 15:23:40 +0600

    load categories data from API

--- src/components/Pages/Shared/LeftNav/LeftNav.jsx
+++ src/components/Pages/Shared/LeftNav/LeftNav.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
+
 const LeftNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  console.log(categories);
   return (
     <div>
-      <h3>This is Left Nav</h3>
+      <h5>All Categories</h5>
     </div>
   );
 };
