91cb86e589a16342bd3135ab9c26b6faffb0270c
tonmoy <tonmoy3673@gmail.com>
2024-01-13 20:57:35 +0600

    added card

--- src/components/Pages/Shared/Categories/Category.jsx
+++ src/components/Pages/Shared/Categories/Category.jsx
@@ -1,10 +1,25 @@
-import { useParams } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const Category = () => {
+  const category = useLoaderData();
+  const { title, details, rating, author, image_url } = category;
+  console.log(category);
   const { id } = useParams();
   return (
     <div>
-      <h2>This is Category {id}</h2>
+      <h2>News Category {category.length}</h2>
+
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
