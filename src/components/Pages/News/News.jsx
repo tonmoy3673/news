d1a1ab06ed8a731e9ebe4dd7411a619743917650
tonmoy <tonmoy3673@gmail.com>
2024-01-14 12:10:49 +0600

    design news card

--- src/components/Pages/News/News.jsx
+++ src/components/Pages/News/News.jsx
@@ -1,18 +1,35 @@
+import moment from "moment/moment";
 import { Card, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FaRegBookmark } from "react-icons/fa6";
+import { FaShareNodes } from "react-icons/fa6";
 
 const News = (news) => {
   const { details, image_url, title, _id, author } = news.news;
   return (
     <div className="py-2">
       <Card>
-        <Card.Header>
-          <Image
-            style={{ height: "40px" }}
-            src={author.img}
-            className="img-fluid"
-            roundedCircle
-          ></Image>
+        <Card.Header className="d-flex justify-content-between align-items-center">
+          <div className="d-flex py-2">
+            <Image
+              style={{ height: "40px" }}
+              src={author.img}
+              className="img-fluid"
+              roundedCircle
+            ></Image>
+            <div className="ms-4 mb-0 text-start flex-grow-1">
+              <p className="mb-0">{author?.name}</p>
+              <p>
+                <small>
+                  {moment(author?.published_date).format("yyyy-MM-D")}
+                </small>
+              </p>
+            </div>
+          </div>
+          <Link>
+            <FaRegBookmark className="me-2" />
+            <FaShareNodes />
+          </Link>
         </Card.Header>
         <Card.Body>
           <Card.Title className="py-1">{title}</Card.Title>
