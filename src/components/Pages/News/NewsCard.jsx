667b6699c917e05734d43a8bfb7dfd1d11d3ffa7
tonmoy <tonmoy3673@gmail.com>
2024-01-14 16:23:32 +0600

    set card

--- src/components/Pages/News/NewsCard.jsx
+++ src/components/Pages/News/NewsCard.jsx
@@ -1,11 +1,21 @@
+import { Card } from "react-bootstrap";
 import { useLoaderData } from "react-router-dom";
 
 const NewsCard = () => {
   const newsInfo = useLoaderData();
   const { title, details, image_url } = newsInfo;
   return (
-    <div>
+    <div className="py-5">
       <h2>This is News Card</h2>
+      <Card>
+        <Card.Body>
+          <Card.Title className="py-2">{title}</Card.Title>
+          <Card.Img variant="top" src={image_url} />
+          <Card.Text className="py-2">{details}</Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
