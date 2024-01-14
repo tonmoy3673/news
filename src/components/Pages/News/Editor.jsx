857c0d034543e11247893a28a4e30878bbaa2176
tonmoy <tonmoy3673@gmail.com>
2024-01-14 17:19:27 +0600

    set editor

---
+++ src/components/Pages/News/Editor.jsx
@@ -0,0 +1,58 @@
+import { Card, Col, Row } from "react-bootstrap";
+import ed1 from "../../../assets/editorsInsight1.png";
+import ed2 from "../../../assets/editorsInsight2.png";
+import ed3 from "../../../assets/editorsInsight3.png";
+import { FaCalendar } from "react-icons/fa6";
+
+const Editor = () => {
+  return (
+    <div>
+      <h4 className="text-center text-secondary py-3">Editors Insight</h4>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Img variant="top" src={ed1} />
+              <Card.Text>
+                The Most Stylish Wedding Guest Dresses For Spring.
+              </Card.Text>
+              <p className="text-center text-muted">
+                <FaCalendar className="me-2" />
+                <small>10 Jan 2024</small>
+              </p>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Img variant="top" src={ed2} />
+              <Card.Text>This is the Most Effective News for world.</Card.Text>
+              <p className="text-center text-muted">
+                <FaCalendar className="me-2" />
+                <small>14 Jan 2024</small>
+              </p>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Img variant="top" src={ed3} />
+
+              <Card.Text className="pt-2">
+                This is the Most Effective News for world.
+              </Card.Text>
+              <p className="text-center text-muted">
+                <FaCalendar className="me-2" />
+                <small>14 Jan 2024</small>
+              </p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Editor;
--- src/components/Pages/News/NewsCard.jsx
+++ src/components/Pages/News/NewsCard.jsx
@@ -1,9 +1,11 @@
-import { Card } from "react-bootstrap";
-import { useLoaderData } from "react-router-dom";
+import { Button, Card } from "react-bootstrap";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { Link, useLoaderData } from "react-router-dom";
+import Editor from "./Editor";
 
 const NewsCard = () => {
   const newsInfo = useLoaderData();
-  const { title, details, image_url } = newsInfo;
+  const { title, details, image_url, category_id } = newsInfo;
   return (
     <div className="py-5">
       <h2>This is News Card</h2>
@@ -12,10 +14,17 @@ const NewsCard = () => {
           <Card.Title className="py-2">{title}</Card.Title>
           <Card.Img variant="top" src={image_url} />
           <Card.Text className="py-2">{details}</Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
+          <Link to={`/category/${category_id}`}>
+            <Button variant="outline-primary">
+              <FaArrowLeftLong className="me-2" />
+              All news in this category
+            </Button>
+          </Link>
         </Card.Body>
       </Card>
+      <div className="py-4">
+        <Editor />
+      </div>
     </div>
   );
 };
