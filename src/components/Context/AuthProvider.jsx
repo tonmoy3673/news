a0c9c253713b1a7ebb6e3f8b1873957278d5f614
tonmoy <tonmoy3673@gmail.com>
2024-01-27 16:57:50 +0600

    set loading

--- src/components/Context/AuthProvider.jsx
+++ src/components/Context/AuthProvider.jsx
@@ -14,19 +14,23 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-
+  const [loading, setLoading] = useState(true);
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const loginUser = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
   const logOut = () => {
+    setLoading(true);
     return signOut(auth);
   };
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
       setUser(loggedUser);
+      return setLoading(false);
     });
     return () => {
       unsubscribe();
@@ -37,6 +41,7 @@ const AuthProvider = ({ children }) => {
     loginUser,
     user,
     logOut,
+    loading,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,11 +1,19 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../Context/AuthProvider";
+import { Spinner } from "react-bootstrap";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
-
+  if (loading) {
+    return (
+      <div className="d-flex mx-auto py-3 text-center">
+        <p className="me-2">Loading...</p>{" "}
+        <Spinner className="text-center" animation="grow" />
+      </div>
+    );
+  }
   if (user) {
     return children;
   }
